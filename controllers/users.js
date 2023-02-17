29c75f8b6be20a20795ec69cdfb73e76480d121b
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-02-17 16:08:47 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -3,13 +3,31 @@ const BadRequestError = require('../errors/BadRequestError')
 const InternalServerError = require('../errors/InternalServerError')
 const NotFoundError = require('../errors/NotFoundError')
 
-
 //GET /users — возвращает всех пользователей
 module.exports.getUsers = (req, res) => {
   User.find({})
   .then(users => res.send({ data: users }))
   .catch((err) => {
     console.log(`Произошла неизвестная ошибка ${err.name}: ${err.message}`);
+  })
+};
+
+//POST /users — создаёт пользователя
+module.exports.postUsers = (req, res) => {
+  const {name,about,avatar} = req.body // получим из объекта запроса данные
+  User.create({name,about,avatar})
+  .then(user => res.send({
+    name: user.name,
+    about: user.about,
+    avatar: user.avatar, }))
+  .catch((err) => {
+    if (err.name == "BadRequestError") {
+      next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+    } else if (err.name == "InternalServerError") {
+      next(new InternalServerError('Ошибка по умолчанию'));
+    } else {
+      next(err);
+    }
   })
 };
 
@@ -27,27 +45,6 @@ module.exports.findUsersById = (req, res) => {
       }
     });
 };
-
-//POST /users — создаёт пользователя
-module.exports.postUsers = (req, res) => {
-  req.user._id
-  const {name,about,avatar} = req.body // получим из объекта запроса данные
-  User.create({name,about,avatar})
-  .then(user => res.send({
-    name: user.name,
-    about: user.about,
-    avatar: user.avatar}))
-  .catch((err) => {
-    if (err.name == "BadRequestError") {
-      next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
-    } else if (err.name == "InternalServerError") {
-      next(new InternalServerError('Ошибка по умолчанию'));
-    } else {
-      next(err);
-    }
-  })
-};
-
 
 // PATCH /users/me — обновляет профиль
 module.exports.patchUsers = (req, res) => {
