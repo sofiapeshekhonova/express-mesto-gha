1f5a8432376e4d3d16ec5fbe014894723bfc7f1c
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-02-17 15:49:14 +0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -13,26 +13,30 @@ module.exports.getUsers = (req, res) => {
   })
 };
 
+
+// const {avatar} = req.body
+// User.findByIdAndUpdate(req.user._id, {avatar}, {new: true})
+// .then((user) => res.send(user))
+
 //POST /users — создаёт пользователя
-module.exports.postUsers = (req, res) => {
-  req.user._id
-  const {name,about,avatar} = req.body // получим из объекта запроса данные
-  User.create({name,about,avatar}, req.user._id)
-  .then(user => res.send({
-    name: user.name,
-    about: user.about,
-    avatar: user.avatar,
-    _id: req.user._id,}))
-  .catch((err) => {
-    if (err.name == "BadRequestError") {
-      next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
-    } else if (err.name == "InternalServerError") {
-      next(new InternalServerError('Ошибка по умолчанию'));
-    } else {
-      next(err);
-    }
-  })
-};
+// module.exports.postUsers = (req, res) => {
+//   req.user._id
+//   const {name,about,avatar} = req.body // получим из объекта запроса данные
+//   User.create({name,about,avatar})
+//   .then(user => res.send({
+//     name: user.name,
+//     about: user.about,
+//     avatar: user.avatar,}))
+//   .catch((err) => {
+//     if (err.name == "BadRequestError") {
+//       next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+//     } else if (err.name == "InternalServerError") {
+//       next(new InternalServerError('Ошибка по умолчанию'));
+//     } else {
+//       next(err);
+//     }
+//   })
+// };
 
 //GET /users/:userId - возвращает пользователя по _id
 module.exports.findUsersById = (req, res) => {
