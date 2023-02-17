6bc7b9620d5fc8627909cc891ec2be633d756cb1
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-02-17 16:04:10 +0300

    refactor users

--- controllers/users.js
+++ controllers/users.js
@@ -13,32 +13,6 @@ module.exports.getUsers = (req, res) => {
   })
 };
 
-
-// const {avatar} = req.body
-// User.findByIdAndUpdate(req.user._id, {avatar}, {new: true})
-// .then((user) => res.send(user))
-
-//POST /users — создаёт пользователя
-module.exports.postUsers = (req, res) => {
-  req.user._id
-  const {name,about,avatar} = req.body // получим из объекта запроса данные
-  User.create({name,about,avatar})
-  .then(user => res.send({
-    name: user.name,
-    about: user.about,
-    avatar: user.avatar,
-    _id: user._id}))
-  .catch((err) => {
-    if (err.name == "BadRequestError") {
-      next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
-    } else if (err.name == "InternalServerError") {
-      next(new InternalServerError('Ошибка по умолчанию'));
-    } else {
-      next(err);
-    }
-  })
-};
-
 //GET /users/:userId - возвращает пользователя по _id
 module.exports.findUsersById = (req, res) => {
   User.findById(req.params.id)
@@ -53,6 +27,27 @@ module.exports.findUsersById = (req, res) => {
       }
     });
 };
+
+//POST /users — создаёт пользователя
+module.exports.postUsers = (req, res) => {
+  req.user._id
+  const {name,about,avatar} = req.body // получим из объекта запроса данные
+  User.create({name,about,avatar})
+  .then(user => res.send({
+    name: user.name,
+    about: user.about,
+    avatar: user.avatar}))
+  .catch((err) => {
+    if (err.name == "BadRequestError") {
+      next(new BadRequestError('Переданы некорректные данные при создании пользователя'));
+    } else if (err.name == "InternalServerError") {
+      next(new InternalServerError('Ошибка по умолчанию'));
+    } else {
+      next(err);
+    }
+  })
+};
+
 
 // PATCH /users/me — обновляет профиль
 module.exports.patchUsers = (req, res) => {
