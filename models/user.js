e235b4482c827504e2f434c5de469a9c6feab376
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-03-06 14:14:39 +0300

    тест неверных логина и пароля

--- models/user.js
+++ models/user.js
@@ -33,21 +33,22 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-userSchema.statics.findUserByCredentials = async function (email, password) {
-  try {
-    const user = await this.findOne({ email }).select('+password');
-    if (!user) {
-      return Promise.reject();
-    }
-    const matched = await bcrypt.compare(password, user.password);
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
 
-    if (!matched) {
-      return Promise.reject();
-    }
-    return user;
-  } catch (err) {
-    return Promise.reject();
-  }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user; // теперь user доступен
+        });
+    });
 };
 
 module.exports = mongoose.model('User', userSchema);
