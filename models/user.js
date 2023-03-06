00d25c7cb0909943b3f1bcf5df7bd31bcd61c244
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-03-06 08:16:50 +0300

    pr 14

--- models/user.js
+++ models/user.js
@@ -1,22 +1,53 @@
+const bcrypt = require('bcrypt');
+const isEmail = require('validator/lib/isEmail');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: [2, 'Must be at least 2 characters.'], // минимальная длина имени — 2 символа
     maxlength: [30, 'Must be at less than 30 characters.'], // а максимальная — 30 символов
   },
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: [2, 'Must be at least 2 characters.'],
     maxlength: [30, 'Must be at less than 30 characters.'],
   },
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+  },
+  email: {
+    type: String,
     required: true,
+    unique: true,
+    validate: [isEmail, 'Неправильный формат почты'],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
   },
 });
+
+userSchema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userSchema);
