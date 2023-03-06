f5e86635ec081f9d7815dc9880da668cefb7c98f
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-03-06 20:08:28 +0300

    лайк ошибки

--- routes/cards.js
+++ routes/cards.js
@@ -12,6 +12,7 @@ const {
 } = require('../controllers/cards');
 
 router.get('/cards', getCards);
+
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30).required(),
@@ -24,7 +25,17 @@ router.delete('/cards/:cardId', celebrate({
     cardId: Joi.string().hex().length(24),
   }),
 }), deleteCards);
-router.put('/cards/:cardId/likes', putLikes);
-router.delete('/cards/:cardId/likes', deleteLikes);
+
+router.put('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), putLikes);
+
+router.delete('/cards/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().hex().length(24),
+  }),
+}), deleteLikes);
 
 module.exports = router;
